8eac267d644deba3e79fd437bc85a87dedcc9634
Marc Hollander <marchollander@gmail.com>
2021-11-02 10:20:56 +0100

    Add the base of the build script

--- web/index.php
+++ web/index.php
@@ -2,48 +2,30 @@
 <html>
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title><?php echo $_SERVER['SERVER_NAME']; ?></title>
-    <style type="text/css" media="screen">
-        html, body {
-            min-height: 100vh;
-        }
-        body {
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            background-color: #eee;
-            font-family: sans-serif;
-            line-height: 2;
-            color: #333;
-        }
-        h1 {
-            font-weight: normal;
-        }
-        .card {
-            margin: 5em auto;
-            padding: 1em;
-            width: 20em;
-            background-color: white;
-            border-radius: 0.5em;
-            text-align: center;
-        }
-        .portret {
-            margin: -1em;
-            width: calc(100% + 2em);
-            border-radius: 0.5em;
-        }
-    </style>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Hi! I'm Marc</title>
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link rel="preload" as="style" href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700,800|Playfair+Display:,300,400,700&display=swap" />
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700,800|Playfair+Display:,300,400,700&display=swap" media="print" onload="this.media='all'" />
+    <link rel="stylesheet" href="assets/css/style.css" />
 </head>
 <body>
-<div class="card">
-    <img alt="A portret of me" class="portret"
-         src="https://s.gravatar.com/avatar/2fafff9b7d608f40a41f6e4eb3b12d5e?s=1024">
-    <h1>Hi! I&rsquo;m Marc</h1>
-    <p>
-        <a href="https://github.com/MGHollander/">GitHub</a>
-        | <a href="https://www.linkedin.com/in/mghollander/">LinkedIn</a>
-    </p>
+<div class="container">
+    <div class="container-item container-portrait"></div>
+
+    <div class="container-item container-content">
+        <h1 class="title">Hi! I&rsquo;m Marc</h1>
+
+        <p>
+            And I&rsquo;m a self-taught developer from the Netherlands.
+        </p>
+
+        <p>
+            <a href="https://github.com/MGHollander/"><img src="assets/img/icon-github.svg" alt="Visit my Github" class="resource-logo"></a>
+            <a href="https://gitlab.com/MGHollander"><img src="assets/img/icon-gitlab-rgb.svg" alt="Visit my GitLab" class="resource-logo"></a>
+            <a href="https://www.linkedin.com/in/mghollander/"><img src="assets/img/icon-linkedin.svg" alt="Visit my LinkedIn" class="resource-logo"></a>
+        </p>
+    </div>
 </div>
 </body>
 </html>
